770a129f58a1056fe7af4719f700cb5f995b4f06
brendojn <brendojnio@gmail.com>
2022-07-04 16:29:26 -0300

    Corrigindo permissão

--- views/template.php
+++ views/template.php
@@ -37,11 +37,11 @@
                         <li><a href="<?php echo BASE_URL; ?>recovery">Recuperação</a></li>
                         <li><a href="<?php echo BASE_URL; ?>projects">Avaliações</a></li>
                         <li><a href="<?php echo BASE_URL; ?>login/sair">Sair</a></li>
-                    <?php elseif (isset($_SESSION['logged']) && !empty($_SESSION['logged']) && ($isLeadSquad == true || $isPo = true)): ?>
-                        <li><a href="<?php echo BASE_URL; ?>projects">Avaliações</a></li>
-                        <li><a href="<?php echo BASE_URL; ?>login/sair">Sair</a></li>
                     <?php elseif (isset($_SESSION['logged']) && !empty($_SESSION['logged']) && $isLead == false): ?>
                         <li><a href="<?php echo BASE_URL; ?>plans">Planos de Estudo</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>login/sair">Sair</a></li>
+                    <?php elseif (isset($_SESSION['logged']) && !empty($_SESSION['logged']) && ($isLeadSquad == true || $isPo = true)): ?>
+                        <li><a href="<?php echo BASE_URL; ?>projects">Avaliações</a></li>
                         <li><a href="<?php echo BASE_URL; ?>login/sair">Sair</a></li>
                     <?php else: ?>
                         <li><a href="<?php echo BASE_URL; ?>add">Cadastre-se</a></li>
